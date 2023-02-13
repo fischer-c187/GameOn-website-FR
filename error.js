9853bdd4d5907c8ab59676ee432d670cc5a3b012
drsky <contact@drsky.ovh>
2023-02-13 11:30:45 +0100

    rework du code js

--- error.js
+++ error.js
@@ -1,3 +1,5 @@
+// contient toutes les informations de chaque input :
+// la clés correspondent à la valeur de l'attribut name de chaque input
 const validation = {
   first: {
     errorMessage: 'Veuillez rentrer deux caractères minimum',
@@ -36,74 +38,146 @@ const validation = {
   },
 };
 
+/**
+ * on ajoute les classes qui permettent de gérer l'affichage de l'erreur,
+ * en css on affiche l'erreur avec un ::after la propriétée content récupère le message d'erreur 
+ * avec la fonction css attr(data-error).
+ * 
+ * On affiche l'erreur sur le parent (<div class="formData">)
+ * 
+ * @param {Element} element élément concerné par l'erreur
+ * @param {string} text message d'erreur 
+ */
 function enableError(element, text) {
   element.parentNode.setAttribute('data-error-visible', true);
   element.parentNode.setAttribute('data-error', text);
   validation[element.name].valid = false;
 }
 
+/**
+ * on désactive l'erreur en modifiant la valeur de l'attribut data-error-visible
+ * 
+ * @param {element} element 
+ */
 function disableError(element) {
   element.parentNode.setAttribute('data-error-visible', false);
   validation[element.name].valid = true;
 }
 
+/**
+ * return le resultat d'un test regex 
+ * 
+ * @param {RegExp} regex 
+ * @param {string} text 
+ * 
+ * @returns {boolean}
+ */
+function regexTest (regex, text) {
+  return regex.test(text)
+}
+
+function checked (element) {
+  return element.checked
+}
+
+/**
+ * fonction de validation pour toutes les inputs de type text, date etc.
+ * on se sert de event.target.name pour récupèrer notre regex dans notre object validation
+ * on test notre regex sur event.target.value
+ * 
+ * @param {Event} event 
+ */
 function validationText(event) {
-  if (!validation[event.target.name].condition.test(event.target.value)) {
-    // textError[eventtarget.name] correspond au texte d'erreur
-    enableError(event.target, validation[event.target.name].errorMessage);
-  } else {
-    // e.target.parentNode.setAttribute('data-error-visible', false)
+  const name = event.target.name
+  const value = event.target.value
+  const regex = validation[name].condition
+
+  if (regexTest(regex, value)) {
     disableError(event.target);
+  } else {
+    enableError(event.target, validation[name].errorMessage);
   }
 }
 
+/**
+ * Concerne la vérification des inputs de type checkbox
+ * 
+ * @param {Event} event 
+ */
 function validationCheck(event) {
-  if (event.target.checked) {
-    validation[event.target.name].valid = true;
+  const name = event.target.name
+
+  if (checked(event.target)) {
     disableError(event.target);
   } else {
-    enableError(event.target, validation[event.target.name].errorMessage);
-    validation[event.target.name].valid = false;
+    enableError(event.target, validation[name].errorMessage);
   }
 }
 
+/**
+ * Concerne la vérification des inputs de type radio, on vérifie que notre input n'avait aucune
+ * case de cochée, il n'est pas possible de désélectionner notre case, c'est inutile de modifier
+ * la valeur de valid si elle est déjà sur true
+ * 
+ * @param {Event} event 
+ */
 function validationRadio(event) {
-  if (event.target.checked && validation[event.target.name].valid == false) {
-    validation[event.target.name].valid = true;
+  const name = event.target.name
+  if (checked(event.target) && validation[name].valid == false) {
     disableError(event.target);
   }
 }
 
+/**
+ * Permet d'initialiser tout nos events sur nos inputs, on verifie si des champs sont déjà remplis 
+ * et on met à jour la valeur de validation.name.valid en conséquence 
+ * 
+ * @param {Object} validationObj correspond à notre object qui contient les conditions, message d'erreur et la validation de l'entrée 
+ * @returns {Array} contient tous les elements séléctionnés 
+ */
 function setEventValidation(validationObj) {
   const elements = [];
   let element = '';
+
   for (let key of Object.keys(validationObj)) {
-    // nous permet de savoir si la condition est une regex
-    if (validationObj[key].condition instanceof RegExp) {
-      element = document.querySelector(`input[name=${key}]`);
-      elements.push(element);
+    let condition = validationObj[key].condition;
+    let selector = `input[name=${key}]`;
+
+    // on vérifie si la condition de notre input est une regex 
+    if (condition instanceof RegExp) {
+      element = document.querySelector(selector);
       element.addEventListener('blur', validationText);
       element.addEventListener('input', validationText);
-    } else if (validationObj[key].condition === 'check') {
-      element = document.querySelector(`input[name=${key}]`);
-      elements.push(element);
+      validationObj[key].valid = regexTest(condition, element.value)
+    }
+    else if (condition === 'check') {
+      element = document.querySelector(selector);
       element.addEventListener('change', validationCheck);
-    } else if (validationObj[key].condition === 'radio') {
-      element = document.querySelectorAll(`input[name=${key}]`);
-      elements.push(element);
+      validationObj[key].valid = checked(element)
+    } 
+    // on ajoute notre événement à tous les input de type radio
+    else if (condition === 'radio') {
+      element = document.querySelectorAll(selector);
       for (let radio of element) {
         radio.addEventListener('change', validationRadio);
+        validationObj[key].valid = (checked(radio) && validationObj[key].valid === false) ? true : validationObj[key].valid;
       }
     }
+
+    elements.push(element)
   }
 
   return elements;
 }
 
+/**
+ * si toutes les entrées du formulaire sont validées alors on affiche le message d'inscription
+ * 
+ * @param {Event} Event reçu lors du click sur le boutton d'inscription
+ */
 function validate(event) {
   event.preventDefault();
 
-  console.log('entre');
   let isValid = true;
   for (let key of Object.keys(validation)) {
     if (!validation[key].valid) {
@@ -115,28 +189,30 @@ function validate(event) {
     }
   }
   if (isValid) {
-    validForm();
+    displaySuccessMessage();
   }
 }
 
-// function hiddenModal() {
-//   modalbg.style.display = 'none'
-// }
+function createElement(tag, className, value) {
+  const element = document.createElement(tag);
+  element.setAttribute('class', className);
+  element.innerText = value;
+  return element;
+}
 
-function validForm() {
+/**
+ * affichage du message de validation de l'inscription
+ */
+function displaySuccessMessage() {
   const modalBody = document.querySelector('.modal-body');
   modalBody.innerHTML = ''
   modalBody.classList.add('modal-body--valid')
-  const text = document.createElement('p');
-  text.classList.add('valid__message');
-  text.innerText = 'Merci ! Votre réservation a été reçue.'
-  modalBody.append(text)
 
-  const btn = document.createElement('button')
-  btn.setAttribute('class', 'btn-close btn--close')
-  btn.innerText = 'Fermer'
-  btn.addEventListener('click', hiddenModal)
-  modalBody.append(btn)
+  const text = createElement('p', 'valid__message', 'Merci ! Votre réservation a été reçue.');
+  const btn = createElement('button', 'btn-close btn--close', 'Fermer');
+
+  btn.addEventListener('click', hiddenModal);
+  modalBody.append(text, btn);
 }
 
 const elements = setEventValidation(validation);
